974df22de240c0dcfe1c52e463a173d3aea524c0
Luca Leyva <luca.leyva@mercurygate.com>
2023-05-17 07:04:10 -0700

    rendered the time of each appointment

--- src/Appointment.js
+++ src/Appointment.js
@@ -2,11 +2,18 @@ import React from "react";
 
 export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
 
+const appointmentTimeofDay = (startsAt) => {
+    const [h, m] = new Date(startsAt).toTimeString().split(":");
+    return `${h}:${m}`;
+}
+
 export const AppointmentsDayView = ( { appointments } ) => ( 
     <div id="appointmentsDayView"> 
     <ol> 
         {appointments.map(appointment => ( 
-            <li key={appointment.startsAt} /> 
+            <li key={appointment.startsAt}>
+                {appointmentTimeofDay(appointment.startsAt)}
+            </li>
         ))} 
     </ol> 
     </div> 
