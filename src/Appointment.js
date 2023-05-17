e5a4cf4b31dc9d79de34f23c2bba6970909c5c02
Luca Leyva <luca.leyva@mercurygate.com>
2023-05-17 06:45:43 -0700

    Rendered basic structure of a list and its list items

--- src/Appointment.js
+++ src/Appointment.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
 
-
-export const AppointmentsDayView = () =>  (
-    <div id="appointmentsDayView">
-        <ol />
-    </div>
+export const AppointmentsDayView = ( { appointments } ) => ( 
+    <div id="appointmentsDayView"> 
+    <ol> 
+        {appointments.map(appointment => ( 
+            <li key={appointment.startsAt} /> 
+        ))} 
+    </ol> 
+    </div> 
 );
-
