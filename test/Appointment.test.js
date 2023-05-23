9ed7ea903ca732170c3919f0aa8dba909e17baaf
Luca Leyva <luca.leyva@mercurygate.com>
2023-05-23 06:57:44 -0700

    select the first appointment by default

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -31,8 +31,12 @@ describe("AppointmentsDayView", () => {
     
   const today = new Date();
   const twoAppointments = [
-    { startsAt: today.setHours(12, 0) },
-    { startsAt: today.setHours(13, 0) },
+    { startsAt: today.setHours(12, 0), 
+      customer: { firstName: "Ashley" }  
+    },
+    { startsAt: today.setHours(13, 0),
+      customer: { firstName: "Ashley" }  
+    },
   ];
 
   beforeEach(() => {
@@ -85,6 +89,14 @@ describe("AppointmentsDayView", () => {
     render(<AppointmentsDayView appointments={[]} />);
 
     expect(document.body.textContent).toContain("There are no appointments scheduled for today.");
+  });
+
+  it("selects the first appointment by default", () => {
+    render(
+      <AppointmentsDayView appointments={twoAppointments} />
+    );
+
+    expect(document.body.textContent).toContain("Ashley")
   });
   
 });
