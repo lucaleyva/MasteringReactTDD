78e622248d2fc6a9ee53e0a3bd6551a26a5ec11b
Patrick Manelski <patrick.manelski@mercurygate.com>
2023-06-28 10:03:27 -0400

    add matcher and tests
    next section is "Learning About Jest's Matcher Utilities"

---
+++ test/matchers/toContainText.js
@@ -0,0 +1,11 @@
+import {matcherHint, printExpected} from "jest-matcher-utils";
+
+export const toContainText = (received, expectedText) => {
+    const pass = received.textContent.includes(expectedText);
+    const message = () => matcherHint("toContainText", "element", printExpected(expectedText), {});
+    return {
+        pass,
+        message
+    };
+};
+
---
+++ test/matchers/toContainText.test.js
@@ -0,0 +1,28 @@
+import {toContainText} from "./toContainText";
+
+
+const stripTerminalColor = (text) => text.replace(/\x1B\[\d+m/g, "");
+
+describe("toContainText matcher", () => {
+    it("returns pass is true when text is found in the given DOM element", () => {
+        const domElement = {textContent: "text to find"};
+        const result = toContainText(domElement, "text to find");
+        expect(result.pass).toBe(true);
+    });
+
+    it('return pass is false when the text is not found in the given DOM element', () => {
+        const domElement = {textContent: ""};
+        const result = toContainText(domElement, 'text to find');
+        expect(result.pass).toBe(false);
+    });
+
+    it("returns a message that contains the source line if no match", () => {
+        const domElement = {textContent: ""};
+        const result = toContainText(domElement, "text to find");
+        expect(stripTerminalColor(result.message())).toContain(`expect(element).toContainText("text to find")`);
+    });
+
+});
+
+
+
